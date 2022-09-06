66c2d8faf3d11147cae736dd485becc821775569
Chare Lampnight <charelampnight@proton.me>
2022-09-05 20:29:17 -0700

    Original version. Clean. No errors.

---
+++ deleteChanges.sh
@@ -0,0 +1,6 @@
+#!/usr/local/bin/bash
+
+Many changes in this file, but I don't want the changes. So, do 'git checkout <name of this file>'.
+
+################################# Add some text after this line before doing git checkout ##############
+
